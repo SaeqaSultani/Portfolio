6608b85cf4c5ea055a06914fc08b36a5a00d49f3
Saeqa Sultani <saeqasultani@gmail.com>
2022-08-08 10:23:25 +0430

    Edaited the form-valid file

---
+++ form-valid.js
@@ -0,0 +1,13 @@
+const form = document.querySelector('.form');
+const text = document.querySelector('.val-text');
+const email = document.querySelector('.email');
+
+form.addEventListener('submit', (v) => {
+  const str = email.value;
+  if (/[a-z]/.test(str)) {
+    text.innerHTML = '';
+  } else {
+    text.innerHTML = 'please use lowercase for the email.';
+    v.preventDefault();
+  }
+});
